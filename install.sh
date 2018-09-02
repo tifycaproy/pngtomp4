4355a5089547c17eadd13ddf17f8e327d186ba08
Carlos Palacios <carlosspf24@gmail.com>
2018-09-01 21:22:19 -0400

    agregando instalador

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#0.- Validacion de existencia de binarios
+#1.- Configurar GDM3
+#2.- Configurar Variables de Entorno (Usuarios AWS, PAth de trabajo)
+#3.- Crear Carpeta de imagenes
+#4.- Se copia el binario en la carpeta del PATH
+
